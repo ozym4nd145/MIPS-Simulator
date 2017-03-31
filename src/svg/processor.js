398cd97542a92538895ae8be35a4db0e27ff7ff9
ozym4nd145 <ozym4nd145@outlook.com>
2017-03-31 19:58:19 +0530

    Completed CSS and JS files

---
+++ src/svg/processor.js
@@ -0,0 +1,14 @@
+console.log("added");
+function add_text()
+{
+    var t1 = document.querySelector("#text_fetch");
+    var t2 = document.querySelector("#text_decode");
+    var t3 = document.querySelector("#text_alu");
+    var t4 = document.querySelector("#text_mem");
+    var t5 = document.querySelector("#text_write");
+    t1.textContent = "Fetch Stage";
+    t2.textContent = "Decode Stage";
+    t3.textContent = "ALU Stage";
+    t4.textContent = "Memory Stage";
+    t5.textContent = "WriteBack Stage";
+}
