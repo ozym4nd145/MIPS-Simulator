020f8c030496944a159fe6bc3897a00c42bc959e
ozym4nd145 <ozym4nd145@outlook.com>
2017-04-19 16:45:23 +0530

    Changed cache structure

--- base_code/code/cache.h
+++ base_code/code/cache.h
@@ -3,7 +3,6 @@
  * cache.h
  */
 
-
 #define TRUE 1
 #define FALSE 0
 
@@ -27,9 +26,9 @@
 #define CACHE_PARAM_WRITEALLOC 7
 #define CACHE_PARAM_NOWRITEALLOC 8
 
-
 /* structure definitions */
-typedef struct cache_line_ {
+typedef struct cache_line_
+{
   unsigned tag;
   int dirty;
 
@@ -37,37 +36,45 @@ typedef struct cache_line_ {
   struct cache_line_ *LRU_prev;
 } cache_line, *Pcache_line;
 
-typedef struct cache_ {
-  int size;			/* cache size */
-  int associativity;		/* cache associativity */
-  int n_sets;			/* number of cache sets */
-  unsigned index_mask;		/* mask to find cache index */
-  int index_mask_offset;	/* number of zero bits in mask */
-  Pcache_line *LRU_head;	/* head of LRU list for each set */
-  Pcache_line *LRU_tail;	/* tail of LRU list for each set */
-  int *set_contents;		/* number of valid entries in set */
-  int contents;			/* number of valid entries in cache */
+typedef struct cache_
+{
+  int size;              /* cache size */
+  int associativity;     /* cache associativity */
+  int n_sets;            /* number of cache sets */
+  unsigned index_mask;   /* mask to find cache index */
+  int index_mask_offset; /* number of zero bits in mask */
+  Pcache_set *cache_set;
+  // Pcache_line *LRU_head; /* head of LRU list for each set */
+  // Pcache_line *LRU_tail; /* tail of LRU list for each set */
+  // int *set_contents;     /* number of valid entries in set */
+  int contents; /* number of valid entries in cache */
 } cache, *Pcache;
 
-typedef struct cache_stat_ {
-  int accesses;			/* number of memory references */
-  int misses;			/* number of cache misses */
-  int replacements;		/* number of misses that cause replacments */
-  int demand_fetches;		/* number of fetches */
-  int copies_back;		/* number of write backs */
+typedef struct cache_stat_
+{
+  int accesses;       /* number of memory references */
+  int misses;         /* number of cache misses */
+  int replacements;   /* number of misses that cause replacments */
+  int demand_fetches; /* number of fetches */
+  int copies_back;    /* number of write backs */
 } cache_stat, *Pcache_stat;
 
+typedef struct cache_set_
+{
+  Pcache_line head;
+  Pcache_line tail;
+  int set_contents;
+} cache_set, *Pcache_set;
 
 /* function prototypes */
 void set_cache_param();
 void init_cache();
 void perform_access();
 void flush();
-void delete();
+void delete ();
 void insert();
 void dump_settings();
 void print_stats();
 
-
 /* macros */
-#define LOG2(x) ((int) rint((log((double) (x))) / (log(2.0))))
+#define LOG2(x) ((int)rint((log((double)(x))) / (log(2.0))))
