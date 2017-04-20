c6ce3b33796e929d174e6c37b001c9b337f0fbc7
ozym4nd145 <ozym4nd145@outlook.com>
2017-04-20 19:41:46 +0530

    Modified test script

--- base_code/code/test.sh
+++ base_code/code/test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 make clean
 make
+make clean
 # rm -rf ../my_outputs
 # mkdir -p ../my_outputs
 
@@ -26,8 +27,8 @@ make
 TEST_DIR="../more_test/traces"
 OUT_DIR="../more_test/outputs"
 MY_OUT_DIR="../more_test/my_outputs"
-rm -rf MY_OUT_DIR
-mkdir -p MY_OUT_DIR
+rm -rf $MY_OUT_DIR
+mkdir -p $MY_OUT_DIR
 
 ./sim $TEST_DIR/public-assoc.trace > $MY_OUT_DIR/public-assoc1.out
 ./sim -bs 16 -us 256 -a 1 -wb -wa $TEST_DIR/public-assoc.trace > $MY_OUT_DIR/public-assoc2.out
@@ -35,10 +36,15 @@ mkdir -p MY_OUT_DIR
 ./sim -bs 16 -us 256 -a 4 -wb -wa $TEST_DIR/public-assoc.trace > $MY_OUT_DIR/public-assoc4.out
 ./sim -bs 16 -us 256 -a 8 -wb -wa $TEST_DIR/public-assoc.trace > $MY_OUT_DIR/public-assoc5.out
 echo "Public Assoc"
+echo "public-assoc1.out"
 diff $OUT_DIR/public-assoc1.out $MY_OUT_DIR/public-assoc1.out
+echo "public-assoc2.out"
 diff $OUT_DIR/public-assoc2.out $MY_OUT_DIR/public-assoc2.out
+echo "public-assoc3.out"
 diff $OUT_DIR/public-assoc3.out $MY_OUT_DIR/public-assoc3.out
+echo "public-assoc4.out"
 diff $OUT_DIR/public-assoc4.out $MY_OUT_DIR/public-assoc4.out
+echo "public-assoc5.out"
 diff $OUT_DIR/public-assoc5.out $MY_OUT_DIR/public-assoc5.out
 echo "-------------------------------------------------------"
 
@@ -48,10 +54,15 @@ echo "-------------------------------------------------------"
 ./sim -bs 4 -us 256 -a 1 -wb -wa $TEST_DIR/public-block.trace > $MY_OUT_DIR/public-block4.out
 ./sim -bs 32 -us 256 -a 1 -wb -wa $TEST_DIR/public-block.trace > $MY_OUT_DIR/public-block5.out
 echo "Public Block"
+echo "public-block1.out"
 diff $OUT_DIR/public-block1.out $MY_OUT_DIR/public-block1.out
+echo "public-block2.out"
 diff $OUT_DIR/public-block2.out $MY_OUT_DIR/public-block2.out
+echo "public-block3.out"
 diff $OUT_DIR/public-block3.out $MY_OUT_DIR/public-block3.out
+echo "public-block4.out"
 diff $OUT_DIR/public-block4.out $MY_OUT_DIR/public-block4.out
+echo "public-block5.out"
 diff $OUT_DIR/public-block5.out $MY_OUT_DIR/public-block5.out
 echo "-------------------------------------------------------"
 
@@ -62,10 +73,15 @@ echo "-------------------------------------------------------"
 ./sim -bs 16 -is 128 -ds 128 -a 1 -wb -wa $TEST_DIR/public-instr.trace > $MY_OUT_DIR/public-instr4.out
 ./sim -bs 16 -is 256 -ds 256 -a 1 -wb -wa $TEST_DIR/public-instr.trace > $MY_OUT_DIR/public-instr5.out
 echo "Public Instr"
+echo "public-instr1.out"
 diff $OUT_DIR/public-instr1.out $MY_OUT_DIR/public-instr1.out
+echo "public-instr2.out"
 diff $OUT_DIR/public-instr2.out $MY_OUT_DIR/public-instr2.out
+echo "public-instr3.out"
 diff $OUT_DIR/public-instr3.out $MY_OUT_DIR/public-instr3.out
+echo "public-instr4.out"
 diff $OUT_DIR/public-instr4.out $MY_OUT_DIR/public-instr4.out
+echo "public-instr5.out"
 diff $OUT_DIR/public-instr5.out $MY_OUT_DIR/public-instr5.out
 echo "-------------------------------------------------------"
 
@@ -76,9 +92,13 @@ echo "-------------------------------------------------------"
 ./sim -bs 16 -us 256 -a 1 -wt -wa $TEST_DIR/public-write.trace > $MY_OUT_DIR/public-write3.out
 ./sim -bs 16 -us 256 -a 1 -wb -nw $TEST_DIR/public-write.trace > $MY_OUT_DIR/public-write4.out
 echo "Public Write"
+echo "public-write1.out"
 diff $OUT_DIR/public-write1.out $MY_OUT_DIR/public-write1.out
+echo "public-write2.out"
 diff $OUT_DIR/public-write2.out $MY_OUT_DIR/public-write2.out
+echo "public-write3.out"
 diff $OUT_DIR/public-write3.out $MY_OUT_DIR/public-write3.out
+echo "public-write4.out"
 diff $OUT_DIR/public-write4.out $MY_OUT_DIR/public-write4.out
 echo "-------------------------------------------------------"
 
@@ -86,8 +106,11 @@ echo "-------------------------------------------------------"
 ./sim $TEST_DIR/spice100.trace > $MY_OUT_DIR/spice100.out
 ./sim $TEST_DIR/spice1000.trace > $MY_OUT_DIR/spice1000.out
 echo "Spice 10 100 1000"
+echo "spice10.out"
 diff $OUT_DIR/spice10.out $MY_OUT_DIR/spice10.out
+echo "spice100.out"
 diff $OUT_DIR/spice100.out $MY_OUT_DIR/spice100.out
+echo "spice1000.out"
 diff $OUT_DIR/spice1000.out $MY_OUT_DIR/spice1000.out
 echo "-------------------------------------------------------"
 
@@ -99,12 +122,19 @@ echo "-------------------------------------------------------"
 ./sim -bs 16 -us 8192 -a 4 -wb -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-assoc7.out
 ./sim -bs 16 -us 8192 -a 8 -wb -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-assoc8.out
 echo "Spice 100000 Assoc"
+echo "spice10000-assoc2.out"
 diff $OUT_DIR/spice10000-assoc2.out $MY_OUT_DIR/spice10000-assoc2.out
+echo "spice10000-assoc3.out"
 diff $OUT_DIR/spice10000-assoc3.out $MY_OUT_DIR/spice10000-assoc3.out
+echo "spice10000-assoc4.out"
 diff $OUT_DIR/spice10000-assoc4.out $MY_OUT_DIR/spice10000-assoc4.out
+echo "spice10000-assoc5.out"
 diff $OUT_DIR/spice10000-assoc5.out $MY_OUT_DIR/spice10000-assoc5.out
+echo "spice10000-assoc6.out"
 diff $OUT_DIR/spice10000-assoc6.out $MY_OUT_DIR/spice10000-assoc6.out
+echo "spice10000-assoc7.out"
 diff $OUT_DIR/spice10000-assoc7.out $MY_OUT_DIR/spice10000-assoc7.out
+echo "spice10000-assoc8.out"
 diff $OUT_DIR/spice10000-assoc8.out $MY_OUT_DIR/spice10000-assoc8.out
 echo "-------------------------------------------------------"
 
@@ -112,8 +142,11 @@ echo "-------------------------------------------------------"
 ./sim -bs 16 -us 256 -a 1 -wt -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-write3.out
 ./sim -bs 16 -us 256 -a 1 -wb -nw $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-write4.out
 echo "Spice 100000 Write"
+echo "spice10000-write2.out"
 diff $OUT_DIR/spice10000-write2.out $MY_OUT_DIR/spice10000-write2.out
+echo "spice10000-write3.out"
 diff $OUT_DIR/spice10000-write3.out $MY_OUT_DIR/spice10000-write3.out
+echo "spice10000-write4.out"
 diff $OUT_DIR/spice10000-write4.out $MY_OUT_DIR/spice10000-write4.out
 echo "-------------------------------------------------------"
 
@@ -125,12 +158,19 @@ echo "-------------------------------------------------------"
 ./sim -bs 16 -is 4096 -ds 4096 -a 1 -wb -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-instr7.out
 ./sim -bs 16 -is 8192 -ds 8192 -a 1 -wb -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-instr8.out
 echo "Spice 100000 Instr"
+echo "spice10000-instr2.out"
 diff $OUT_DIR/spice10000-instr2.out $MY_OUT_DIR/spice10000-instr2.out
+echo "spice10000-instr3.out"
 diff $OUT_DIR/spice10000-instr3.out $MY_OUT_DIR/spice10000-instr3.out
+echo "spice10000-instr4.out"
 diff $OUT_DIR/spice10000-instr4.out $MY_OUT_DIR/spice10000-instr4.out
+echo "spice10000-instr5.out"
 diff $OUT_DIR/spice10000-instr5.out $MY_OUT_DIR/spice10000-instr5.out
+echo "spice10000-instr6.out"
 diff $OUT_DIR/spice10000-instr6.out $MY_OUT_DIR/spice10000-instr6.out
+echo "spice10000-instr7.out"
 diff $OUT_DIR/spice10000-instr7.out $MY_OUT_DIR/spice10000-instr7.out
+echo "spice10000-instr8.out"
 diff $OUT_DIR/spice10000-instr8.out $MY_OUT_DIR/spice10000-instr8.out
 echo "-------------------------------------------------------"
 
@@ -141,10 +181,16 @@ echo "-------------------------------------------------------"
 ./sim -bs 64 -us 8192 -a 1 -wb -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-block6.out
 ./sim -bs 128 -us 8192 -a 1 -wb -wa $TEST_DIR/spice10000.trace > $MY_OUT_DIR/spice10000-block7.out
 echo "Spice 100000 Block"
+echo "spice10000-block2.out"
 diff $OUT_DIR/spice10000-block2.out $MY_OUT_DIR/spice10000-block2.out
+echo "spice10000-block3.out"
 diff $OUT_DIR/spice10000-block3.out $MY_OUT_DIR/spice10000-block3.out
+echo "spice10000-block4.out"
 diff $OUT_DIR/spice10000-block4.out $MY_OUT_DIR/spice10000-block4.out
+echo "spice10000-block5.out"
 diff $OUT_DIR/spice10000-block5.out $MY_OUT_DIR/spice10000-block5.out
+echo "spice10000-block6.out"
 diff $OUT_DIR/spice10000-block6.out $MY_OUT_DIR/spice10000-block6.out
+echo "spice10000-block7.out"
 diff $OUT_DIR/spice10000-block7.out $MY_OUT_DIR/spice10000-block7.out
 echo "-------------------------------------------------------"
