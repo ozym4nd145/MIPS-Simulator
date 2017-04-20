19fb56649eace5581158e7dfb8d51b26643e67bb
ozym4nd145 <ozym4nd145@outlook.com>
2017-04-20 18:24:15 +0530

    modified test.sh

--- base_code/code/test.sh
+++ base_code/code/test.sh
@@ -1,19 +1,22 @@
 #! /bin/bash
 make clean
 make
-./sim ../traces/public-assoc.trace > ../outputs/my_public-assoc.out
-./sim ../traces/public-block.trace > ../outputs/my_public-block.out
-./sim ../traces/public-instr.trace > ../outputs/my_public-instr.out
-./sim ../traces/public-write.trace > ../outputs/my_public-write.out
-./sim ../traces/spice10.trace > ../outputs/my_spice10.out
-./sim ../traces/spice100.trace > ../outputs/my_spice100.out
-./sim ../traces/spice1000.trace > ../outputs/my_spice1000.out
+rm -rf ../my_outputs
+mkdir -p ../my_outputs
 
-diff ../outputs/public-assoc1.out ../outputs/my_public-assoc.out
-diff ../outputs/public-block1.out ../outputs/my_public-block.out
-diff ../outputs/public-instr1.out ../outputs/my_public-instr.out
-diff ../outputs/public-write1.out ../outputs/my_public-write.out
-diff ../outputs/spice10.out ../outputs/my_spice10.out
-diff ../outputs/spice100.out ../outputs/my_spice100.out
-diff ../outputs/spice1000.out ../outputs/my_spice1000.out
+./sim ../traces/public-assoc.trace > ../my_outputs/public-assoc.out
+./sim ../traces/public-block.trace > ../my_outputs/public-block.out
+./sim ../traces/public-instr.trace > ../my_outputs/public-instr.out
+./sim ../traces/public-write.trace > ../my_outputs/public-write.out
+./sim ../traces/spice10.trace > ../my_outputs/spice10.out
+./sim ../traces/spice100.trace > ../my_outputs/spice100.out
+./sim ../traces/spice1000.trace > ../my_outputs/spice1000.out
+
+diff ../outputs/public-assoc1.out ../my_outputs/public-assoc.out
+diff ../outputs/public-block1.out ../my_outputs/public-block.out
+diff ../outputs/public-instr1.out ../my_outputs/public-instr.out
+diff ../outputs/public-write1.out ../my_outputs/public-write.out
+diff ../outputs/spice10.out ../my_outputs/spice10.out
+diff ../outputs/spice100.out ../my_outputs/spice100.out
+diff ../outputs/spice1000.out ../my_outputs/spice1000.out
 
